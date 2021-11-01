f2c6dab99d866ab7627e11dd10fa4a2910af035d
kasunicts48 <kasundananjayaweerasooriya@gmail.com>
2021-11-02 05:09:10 +0530

    adding refix return getStaticProps  index.js

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,37 @@
-import { createClient } from "contentful"
+import { createClient } from "contentful";
 import RecipeCard from "../Components/RecipeCard";
 
-export async function getStaticProps(){
-
+export async function getStaticProps() {
   const client = createClient({
     space: process.env.CONTENTFUL_SPACE_ID,
     accessToken: process.env.CONTENTFUL_ACCESS_KEY,
-  })
+  });
 
-  const res = await client.getEntries({content_type : "recipe"});
+  const res = await client.getEntries({ content_type: "recipe" });
 
   return {
-    props : {
-      recipes : res.items,
+      props: {recipes :res.items},
       revalidate: 1,
-    }
-  }
+  };
 }
 
-export default function Home({recipes}) {
+export default function Home({ recipes }) {
   // console.log(recipes)
   return (
     <div className="recipe-list">
-      {recipes.map((val,key) => {
-        return (
-          <RecipeCard key={key} recipes={val} />
-        )
+      {recipes.map((val, key) => {
+        return <RecipeCard key={key} recipes={val} />;
       })}
 
-      <style jsx>{`
-        .recipe-list{
-          display: grid;
-          grid-template-columns: 1fr 1fr;
-          grid-gap: 20px 60px;
-        }
-      `} 
-    </style>
+      <style jsx>
+        {`
+          .recipe-list {
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+            grid-gap: 20px 60px;
+          }
+        `}
+      </style>
     </div>
-  )
+  );
 }
